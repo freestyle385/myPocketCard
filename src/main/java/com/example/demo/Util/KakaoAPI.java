975a6b5e759cd2793cbc3b6643b46c52cfaf656a
hyun0547 <hyun0547@gmail.com>
2022-01-11 16:16:43 +0900

    카카오 로그인 메서드 구현

--- src/main/java/com/example/demo/Util/KakaoAPI.java
+++ src/main/java/com/example/demo/Util/KakaoAPI.java
@@ -8,8 +8,12 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 public class KakaoAPI {
-	public String getAccessToken(String code) {
+	public static String getAccessToken(String code) {
 		String accessToken = "";
 		String refreshToken = "";
 		String reqURL = "https://kauth.kakao.com/oauth/token";
@@ -23,8 +27,8 @@ public class KakaoAPI {
 			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
 			StringBuilder sb = new StringBuilder();
 			sb.append("grant_type=authorization_code");
-			sb.append("&client_id=7d7347d96c6a93fb0e1d7d42adcb3579");
-			sb.append("&redirect_uri=http://localhost:8080/login");
+			sb.append("&client_id=477d92a75a81ced855b4f349c5380bf4");
+			sb.append("&redirect_uri=http://localhost:8081");
 			sb.append("&code="+code);
 			
 			bw.write(sb.toString());
@@ -54,6 +58,72 @@ public class KakaoAPI {
 			e.printStackTrace();
 		}
 		return accessToken;
+	}
+
+	
+	public static HashMap<String, Object> getUserInfo(String accessToken) {
+		HashMap<String, Object> userInfo = new HashMap<String, Object>();
+		String reqUrl = "https://kapi.kakao.com/v2/user/me";
+		try {
+			URL url = new URL(reqUrl);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Authorization", "Bearer " + accessToken);
+			int responseCode = conn.getResponseCode();
+			System.out.println("responseCode =" + responseCode);
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			
+			String line = "";
+			String result = "";
+			
+			while((line = br.readLine()) != null) {
+				result += line;
+			}
+			System.out.println("response body ="+result);
+			
+			JsonParser parser = new JsonParser();
+			JsonElement element =  parser.parse(result);
+			
+			JsonObject properties = element.getAsJsonObject().get("properties").getAsJsonObject();
+			JsonObject kakaoAccount = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
+			
+			String nickname = properties.getAsJsonObject().get("nickname").getAsString();
+			String email = kakaoAccount.getAsJsonObject().get("email").getAsString();
+			
+			userInfo.put("nickname", nickname);
+			userInfo.put("email", email);
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return userInfo;
+	}
+
+
+	public void kakaoLogout(String accessToken) {
+		String reqURL = "http://kapi.kakao.com/v1/user/logout";
+		try {
+			URL url = new URL(reqURL);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Authorization", "Bearer " + accessToken);
+			int responseCode = conn.getResponseCode();
+			System.out.println("responseCode = " + responseCode);
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			
+			String result = "";
+			String line = "";
+			
+			while((line = br.readLine()) != null) {
+				result+=line;
+			}
+			System.out.println(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -1,7 +1,14 @@
 package com.example.demo.controller;
 
+import java.util.HashMap;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.Util.KakaoAPI;
 import com.example.demo.dto.ResultData;
 import com.example.demo.service.MemberService;
 
@@ -21,9 +28,28 @@ public class MemberController {
 	
 	
 	@RequestMapping("/usr/member/doLogin")
-	public String doLogin(String infoOrigin, String userEmail, String userName) {
+	public String doLogin(HttpSession session, String code) {
 		
-		ResultData<String> joinRd = memberService.doJoin(infoOrigin, userEmail, userName);
+		// 1번 인증코드 요청 전달
+		String accessToken = KakaoAPI.getAccessToken(code);
+		// 2번 인증코드로 토큰 전달
+		HashMap<String, Object> userInfo = KakaoAPI.getUserInfo(accessToken);
+		
+		System.out.println("login info : " + userInfo.toString());
+		
+		String userEmail = null;
+		String userNickname = null;
+		
+		if(userInfo.get("email") != null) {
+			userEmail = (String) userInfo.get("email");
+			userNickname = (String) userInfo.get("nickname");
+			
+			session.setAttribute("userId", userEmail);
+			session.setAttribute("accessToken", accessToken);
+			session.setAttribute("userNickname", userNickname);
+		}
+		
+		ResultData<String> joinRd = memberService.doJoin("kakao", userEmail, userNickname);
 		
 		return "/usr/home/main";
 	}
